c6c3dcbd9eac7dbcbbbb45df4fc039e9506c32d8
Tu <bnjamit@88665a0c58cd.ant.amazon.com>
2021-06-04 10:15:21 -0700

    get traitname

--- smithy-crt/src/main/kotlin/software/amazon/smithy/crt/python/Util.kt
+++ smithy-crt/src/main/kotlin/software/amazon/smithy/crt/python/Util.kt
@@ -5,7 +5,7 @@ import software.amazon.smithy.model.shapes.StructureShape
 import software.amazon.smithy.model.traits.Trait.getIdiomaticTraitName
 import java.lang.StringBuilder
 
-const val POINTER_TRAIT = "com.aws.ffi#pointer"
+const val POINTER_TRAIT = "com.aws.ffi#ctype"
 
 fun <T> Optional<T>.orNull(): T? = this.orElse(null)
 
@@ -16,6 +16,9 @@ fun processInput(struct: StructureShape?): String? {
 
     val pointer = struct.findTrait(POINTER_TRAIT).orNull()
     if (pointer != null) {
+        println(pointer.toNode())
+        println("im hereadfdslgnfo")
+        println(pointer.toNode().expectObjectNode().expectStringMember("typename"))
         println(getIdiomaticTraitName(POINTER_TRAIT))
         return ""
     }
